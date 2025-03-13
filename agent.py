20bd3cc5defd920cfd5c6fff6f9a5e434a5601b1
LukasWarnecke <729446@student.inholland.nl>
2025-03-14 00:06:18 +0100

    worked on agent.py and prompts.py

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ from livekit.agents.multimodal import MultimodalAgent
 from livekit.plugins import openai
 from dotenv import load_dotenv
 from api import AssistantFnc
-from prompts import WELCOME_MESSAGE, INSTRUCTIONS
+from prompts import WELCOME_MESSAGE, INSTRUCTIONS, LOOKUP_VIN_MESSAGE
 import os
 
 load_dotenv()
@@ -46,6 +46,36 @@ async def entrypoint(ctx: JobContext):
         )
     )
     session.response.create()
+
+    #defining branches llm can possibly go into
+    @session.on("user_speech_committed")
+    def on_user_speech_committed(msg: llm.ChatMessage):
+        #if user submits image then put str image to message content
+        if isinstance(msg.content, list):
+            msg.content = "\n".join("[image]" if isinstance(x, llm.ChatImage) else x for x in msg)
+
+        if assistant_fnc.has_car():
+            handle_query(msg)
+        else:
+            find_profile(msg)
+    
+    def find_profile(msg: llm.ChatMessage):
+        session.conversation.item.create(
+            llm.ChatMessage(
+                role="system",
+                content=LOOKUP_VIN_MESSAGE(msg)
+            )
+        )
+        session.response.create()
+        
+    def handle_query(msg: llm.ChatMessage):
+        session.conversation.item.create(
+            llm.ChatMessage(
+                role="user",
+                content=msg.content
+            )
+        )
+        session.response.create()
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
